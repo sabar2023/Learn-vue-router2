de208b905b65d29fd491b2fd194d6aecd8a1625d
sabar2023 <137174408+sabar2023@users.noreply.github.com>
2024-09-26 12:28:56 +0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+import { createRouter, createwebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createwebHistory(import.meta.env.BASE_URL),
+  routers: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/Home.vue"),
+    },
+
+    {
+      path: "/",
+      name: "about",
+      component: () => import("../views/About.vue"),
+    },
+  ],
+});
+
+export default router;
 
